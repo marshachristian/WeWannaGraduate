429ebab14e3bf408a42ba73fbbb96003d53121e8
marsha christian <bgq15@txstate.edu>
2026-03-23 17:44:16 -0500

    Bishop piece implementation

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -1,5 +1,19 @@
 package pieces;
 
-public class Bishop {
-    
+import utils.Position;
+import board.Board;
+
+public class Bishop extends Piece {
+    public Bishop(String color, Position position) { super(color, position); }
+
+    @Override
+    public String getSymbol() {
+        return color.equals("white") ? "wp" : "bp";
+    }
+
+    @Override
+    public boolean isValidMove(Position newPos, Board board) {
+        
+        return true; 
+    }
 }
